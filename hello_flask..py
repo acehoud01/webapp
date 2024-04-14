671ff0d5001e524550baa17b8de19e467f600d9e
Anele Bilikwa <aceamahle1@gmail.com>
2024-04-14 18:07:14 +0200

    updated

--- hello_flask..py
+++ hello_flask..py
@@ -1,11 +1,17 @@
 from flask import Flask
+from vsearch import search4letters
 
 app = Flask(__name__)
 
 
+# @app.route('/')
+# def hello() -> str:
+#     return 'Hello world from Flask!'
+
+
 @app.route('/')
-def hello() -> str:
-    return 'Hello world from Flask!'
+def do_search() -> str:
+    return str(search4letters('life, the universe and everything', 'aeiou'))
 
 
 app.run()
